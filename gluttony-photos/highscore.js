20152229b5bf12ebef35ece4e28e225a3880fcc3
jsdrever <jsdrever09@gmail.com>
2022-12-30 15:56:56 -0600

    overload

---
+++ gluttony-photos/highscore.js
@@ -0,0 +1,3 @@
+//needs to display text input for their info
+//needs to display a champions list top 10
+//play again doesn't work
--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//make a startGame function
+//make a timer setTimeout(myFunction, milliseconds, arg(s))
+
+function timeOut() {
+    setTimeout(setAlert, 400000);
+}
+function setAlert() {
+  alert("Time's Up!");
+}
 const startButton = document.getElementById('start-btn')
 
 startButton.addEventListener('click', startGame)
@@ -13,3 +22,38 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+let questions = [
+    {
+        question: "what is 2 + 2?",
+        choice1: "2",
+        choice2: "4",
+        choice3: "5",
+        choice4: "8",
+        answer: 2,
+    },
+    {
+        question: "what is my dogs name?",
+        choice1: "Riley",
+        choice2: "Dogmeat",
+        choice3: "Pookie",
+        choice4: "Cuddle-Bug",
+        answer: 1,
+    },
+    {
+        question: "Is my dog a good boy?",
+        choice1: "nah, he's fat",
+        choice2: "ew, I don't like him",
+        choice3: "He's ok, I guess...",
+        choice4: "OMG! Yes. I love him.",
+        answer: 4,
+    },
+    {
+        question: "what this educational?",
+        choice1: "91.412 million miles",
+        choice2: "7.837 billion as of 2021",
+        choice3: "It was educational for you to build this and that's what really mattered",
+        choice4: "The Triassic Period",
+        answer: 3,
+    },
+]
